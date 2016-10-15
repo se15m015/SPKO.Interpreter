fdb8af9427e6b5301c6b8178f8660b01ed9161f6
richie <se15m015@technikum-wien.at>
2016-10-15 10:21:19 +0200

    added Interpreter

---
+++ src/main/java/at/technikum/wien/figl/winterhalder/interpreter/Interpreter.java
@@ -0,0 +1,15 @@
+package at.technikum.wien.figl.winterhalder.interpreter;
+
+import at.technikum.wien.figl.winterhalder.spko.postfixcalculator.PostfixCalculator;
+import at.technikum.wien.figl.winterhalder.spko.postfixcalculator.PostfixCalculatorTest;
+
+/**
+ * Created by richie on 14/10/16.
+ */
+public class Interpreter {
+    public static void main(String[] args) {
+
+        PostfixCalculator pc = new PostfixCalculator();
+        pc.test();
+    }
+}
